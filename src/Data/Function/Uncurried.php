c4d6919874329ed66752576d983c0405d1b6b030
0x1 <kevin.francart3@gmail.com>
2026-07-05 19:41:32 +0200

    feat: Add PHP FFI implementations

---
+++ src/Data/Function/Uncurried.php
@@ -0,0 +1,53 @@
+<?php
+if (!function_exists('phpurs_uncurry2')) {
+function phpurs_uncurry2($fn) {
+    return function($a, $b = null) use ($fn) {
+        if (func_num_args() < 2) {
+            $__args = func_get_args();
+            return function(...$more) use ($fn, $__args) {
+                return phpurs_uncurry2($fn)(...array_merge($__args, $more));
+            };
+        }
+        return $fn($a)($b);
+    };
+}
+function phpurs_uncurry3($fn) {
+    return function($a, $b = null, $c = null) use ($fn) {
+        if (func_num_args() < 3) {
+            $__args = func_get_args();
+            return function(...$more) use ($fn, $__args) {
+                return phpurs_uncurry3($fn)(...array_merge($__args, $more));
+            };
+        }
+        return $fn($a)($b)($c);
+    };
+}
+function phpurs_uncurry4($fn) {
+    return function($a, $b = null, $c = null, $d = null) use ($fn) {
+        if (func_num_args() < 4) {
+            $__args = func_get_args();
+            return function(...$more) use ($fn, $__args) {
+                return phpurs_uncurry4($fn)(...array_merge($__args, $more));
+            };
+        }
+        return $fn($a)($b)($c)($d);
+    };
+}
+function phpurs_uncurry5($fn) {
+    return function($a, $b = null, $c = null, $d = null, $e = null) use ($fn) {
+        if (func_num_args() < 5) {
+            $__args = func_get_args();
+            return function(...$more) use ($fn, $__args) {
+                return phpurs_uncurry5($fn)(...array_merge($__args, $more));
+            };
+        }
+        return $fn($a)($b)($c)($d)($e);
+    };
+}
+}
+
+
+
+
+$Data_Function_Uncurried_runFn2 = phpurs_uncurry3(function($fn) { return function($a) use(&$fn) { return function($b) use(&$fn, &$a) { return $fn($a, $b); }; }; });
+$Data_Function_Uncurried_runFn3 = phpurs_uncurry4(function($fn) { return function($a) use(&$fn) { return function($b) use(&$fn, &$a) { return function($c) use(&$fn, &$a, &$b) { return $fn($a, $b, $c); }; }; }; });
