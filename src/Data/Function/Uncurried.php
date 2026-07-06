e607a4dabfd125554fa29388374da74bffb16cfa
0x1 <kevin.francart3@gmail.com>
2026-07-06 18:59:09 +0200

    feat: migrate FFI to exports array format

--- src/Data/Function/Uncurried.php
+++ src/Data/Function/Uncurried.php
@@ -1,22 +1,26 @@
 <?php
 
-$Data_Function_Uncurried_runFn2 = function($fn, $a = null, $b = null) {
+$runFn2 = function($fn, $a = null, $b = null) use (&$runFn2) {
     if (func_num_args() < 3) {
         $__args = func_get_args();
-        return function(...$more) use ($__args) {
-            global $Data_Function_Uncurried_runFn2;
-            return $Data_Function_Uncurried_runFn2(...array_merge($__args, $more));
+        return function(...$more) use ($__args, &$runFn2) {
+
+            return $runFn2(...array_merge($__args, $more));
         };
     }
     return $fn($a, $b);
 };
-$Data_Function_Uncurried_runFn3 = function($fn, $a = null, $b = null, $c = null) {
+$runFn3 = function($fn, $a = null, $b = null, $c = null) use (&$runFn3) {
     if (func_num_args() < 4) {
         $__args = func_get_args();
-        return function(...$more) use ($__args) {
-            global $Data_Function_Uncurried_runFn3;
-            return $Data_Function_Uncurried_runFn3(...array_merge($__args, $more));
+        return function(...$more) use ($__args, &$runFn3) {
+
+            return $runFn3(...array_merge($__args, $more));
         };
     }
     return $fn($a, $b, $c);
 };
+
+$exports['runFn2'] = $runFn2;
+$exports['runFn3'] = $runFn3;
+return $exports;
