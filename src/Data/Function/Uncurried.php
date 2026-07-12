7f9ec35a175a0729dd35204418ba392b73c9dafc
0x1 <kevin.francart3@gmail.com>
2026-07-12 17:39:31 +0200

    [tmp] add slash to native PHP functions for OPCache

--- src/Data/Function/Uncurried.php
+++ src/Data/Function/Uncurried.php
@@ -1,10 +1,10 @@
 <?php
 
 $mkFn0 = function($fn) use (&$mkFn0) {
-    if (func_num_args() < 1) {
-        $__args = func_get_args();
+    if (\func_num_args() < 1) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$mkFn0) {
-            return $mkFn0(...array_merge($__args, $more));
+            return $mkFn0(...\array_merge($__args, $more));
         };
     }
     return function() use ($fn) {
@@ -12,10 +12,10 @@ $mkFn0 = function($fn) use (&$mkFn0) {
     };
 };
 $mkFn1 = function($fn) use (&$mkFn1) {
-    if (func_num_args() < 1) {
-        $__args = func_get_args();
+    if (\func_num_args() < 1) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$mkFn1) {
-            return $mkFn1(...array_merge($__args, $more));
+            return $mkFn1(...\array_merge($__args, $more));
         };
     }
     return function($a0 = null) use ($fn) {
@@ -23,10 +23,10 @@ $mkFn1 = function($fn) use (&$mkFn1) {
     };
 };
 $mkFn2 = function($fn) use (&$mkFn2) {
-    if (func_num_args() < 1) {
-        $__args = func_get_args();
+    if (\func_num_args() < 1) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$mkFn2) {
-            return $mkFn2(...array_merge($__args, $more));
+            return $mkFn2(...\array_merge($__args, $more));
         };
     }
     return function($a0 = null, $a1 = null) use ($fn) {
@@ -34,10 +34,10 @@ $mkFn2 = function($fn) use (&$mkFn2) {
     };
 };
 $mkFn3 = function($fn) use (&$mkFn3) {
-    if (func_num_args() < 1) {
-        $__args = func_get_args();
+    if (\func_num_args() < 1) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$mkFn3) {
-            return $mkFn3(...array_merge($__args, $more));
+            return $mkFn3(...\array_merge($__args, $more));
         };
     }
     return function($a0 = null, $a1 = null, $a2 = null) use ($fn) {
@@ -45,10 +45,10 @@ $mkFn3 = function($fn) use (&$mkFn3) {
     };
 };
 $mkFn4 = function($fn) use (&$mkFn4) {
-    if (func_num_args() < 1) {
-        $__args = func_get_args();
+    if (\func_num_args() < 1) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$mkFn4) {
-            return $mkFn4(...array_merge($__args, $more));
+            return $mkFn4(...\array_merge($__args, $more));
         };
     }
     return function($a0 = null, $a1 = null, $a2 = null, $a3 = null) use ($fn) {
@@ -56,10 +56,10 @@ $mkFn4 = function($fn) use (&$mkFn4) {
     };
 };
 $mkFn5 = function($fn) use (&$mkFn5) {
-    if (func_num_args() < 1) {
-        $__args = func_get_args();
+    if (\func_num_args() < 1) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$mkFn5) {
-            return $mkFn5(...array_merge($__args, $more));
+            return $mkFn5(...\array_merge($__args, $more));
         };
     }
     return function($a0 = null, $a1 = null, $a2 = null, $a3 = null, $a4 = null) use ($fn) {
@@ -67,10 +67,10 @@ $mkFn5 = function($fn) use (&$mkFn5) {
     };
 };
 $mkFn6 = function($fn) use (&$mkFn6) {
-    if (func_num_args() < 1) {
-        $__args = func_get_args();
+    if (\func_num_args() < 1) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$mkFn6) {
-            return $mkFn6(...array_merge($__args, $more));
+            return $mkFn6(...\array_merge($__args, $more));
         };
     }
     return function($a0 = null, $a1 = null, $a2 = null, $a3 = null, $a4 = null, $a5 = null) use ($fn) {
@@ -78,10 +78,10 @@ $mkFn6 = function($fn) use (&$mkFn6) {
     };
 };
 $mkFn7 = function($fn) use (&$mkFn7) {
-    if (func_num_args() < 1) {
-        $__args = func_get_args();
+    if (\func_num_args() < 1) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$mkFn7) {
-            return $mkFn7(...array_merge($__args, $more));
+            return $mkFn7(...\array_merge($__args, $more));
         };
     }
     return function($a0 = null, $a1 = null, $a2 = null, $a3 = null, $a4 = null, $a5 = null, $a6 = null) use ($fn) {
@@ -89,10 +89,10 @@ $mkFn7 = function($fn) use (&$mkFn7) {
     };
 };
 $mkFn8 = function($fn) use (&$mkFn8) {
-    if (func_num_args() < 1) {
-        $__args = func_get_args();
+    if (\func_num_args() < 1) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$mkFn8) {
-            return $mkFn8(...array_merge($__args, $more));
+            return $mkFn8(...\array_merge($__args, $more));
         };
     }
     return function($a0 = null, $a1 = null, $a2 = null, $a3 = null, $a4 = null, $a5 = null, $a6 = null, $a7 = null) use ($fn) {
@@ -100,10 +100,10 @@ $mkFn8 = function($fn) use (&$mkFn8) {
     };
 };
 $mkFn9 = function($fn) use (&$mkFn9) {
-    if (func_num_args() < 1) {
-        $__args = func_get_args();
+    if (\func_num_args() < 1) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$mkFn9) {
-            return $mkFn9(...array_merge($__args, $more));
+            return $mkFn9(...\array_merge($__args, $more));
         };
     }
     return function($a0 = null, $a1 = null, $a2 = null, $a3 = null, $a4 = null, $a5 = null, $a6 = null, $a7 = null, $a8 = null) use ($fn) {
@@ -111,10 +111,10 @@ $mkFn9 = function($fn) use (&$mkFn9) {
     };
 };
 $mkFn10 = function($fn) use (&$mkFn10) {
-    if (func_num_args() < 1) {
-        $__args = func_get_args();
+    if (\func_num_args() < 1) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$mkFn10) {
-            return $mkFn10(...array_merge($__args, $more));
+            return $mkFn10(...\array_merge($__args, $more));
         };
     }
     return function($a0 = null, $a1 = null, $a2 = null, $a3 = null, $a4 = null, $a5 = null, $a6 = null, $a7 = null, $a8 = null, $a9 = null) use ($fn) {
@@ -122,100 +122,100 @@ $mkFn10 = function($fn) use (&$mkFn10) {
     };
 };
 $runFn0 = function($fn = null) use (&$runFn0) {
-    if (func_num_args() < 1) {
-        $__args = func_get_args();
+    if (\func_num_args() < 1) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$runFn0) {
-            return $runFn0(...array_merge($__args, $more));
+            return $runFn0(...\array_merge($__args, $more));
         };
     }
     return $fn();
 };
 $runFn1 = function($fn = null, $a0 = null) use (&$runFn1) {
-    if (func_num_args() < 2) {
-        $__args = func_get_args();
+    if (\func_num_args() < 2) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$runFn1) {
-            return $runFn1(...array_merge($__args, $more));
+            return $runFn1(...\array_merge($__args, $more));
         };
     }
     return $fn($a0);
 };
 $runFn2 = function($fn = null, $a0 = null, $a1 = null) use (&$runFn2) {
-    if (func_num_args() < 3) {
-        $__args = func_get_args();
+    if (\func_num_args() < 3) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$runFn2) {
-            return $runFn2(...array_merge($__args, $more));
+            return $runFn2(...\array_merge($__args, $more));
         };
     }
     return $fn($a0, $a1);
 };
 $runFn3 = function($fn = null, $a0 = null, $a1 = null, $a2 = null) use (&$runFn3) {
-    if (func_num_args() < 4) {
-        $__args = func_get_args();
+    if (\func_num_args() < 4) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$runFn3) {
-            return $runFn3(...array_merge($__args, $more));
+            return $runFn3(...\array_merge($__args, $more));
         };
     }
     return $fn($a0, $a1, $a2);
 };
 $runFn4 = function($fn = null, $a0 = null, $a1 = null, $a2 = null, $a3 = null) use (&$runFn4) {
-    if (func_num_args() < 5) {
-        $__args = func_get_args();
+    if (\func_num_args() < 5) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$runFn4) {
-            return $runFn4(...array_merge($__args, $more));
+            return $runFn4(...\array_merge($__args, $more));
         };
     }
     return $fn($a0, $a1, $a2, $a3);
 };
 $runFn5 = function($fn = null, $a0 = null, $a1 = null, $a2 = null, $a3 = null, $a4 = null) use (&$runFn5) {
-    if (func_num_args() < 6) {
-        $__args = func_get_args();
+    if (\func_num_args() < 6) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$runFn5) {
-            return $runFn5(...array_merge($__args, $more));
+            return $runFn5(...\array_merge($__args, $more));
         };
     }
     return $fn($a0, $a1, $a2, $a3, $a4);
 };
 $runFn6 = function($fn = null, $a0 = null, $a1 = null, $a2 = null, $a3 = null, $a4 = null, $a5 = null) use (&$runFn6) {
-    if (func_num_args() < 7) {
-        $__args = func_get_args();
+    if (\func_num_args() < 7) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$runFn6) {
-            return $runFn6(...array_merge($__args, $more));
+            return $runFn6(...\array_merge($__args, $more));
         };
     }
     return $fn($a0, $a1, $a2, $a3, $a4, $a5);
 };
 $runFn7 = function($fn = null, $a0 = null, $a1 = null, $a2 = null, $a3 = null, $a4 = null, $a5 = null, $a6 = null) use (&$runFn7) {
-    if (func_num_args() < 8) {
-        $__args = func_get_args();
+    if (\func_num_args() < 8) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$runFn7) {
-            return $runFn7(...array_merge($__args, $more));
+            return $runFn7(...\array_merge($__args, $more));
         };
     }
     return $fn($a0, $a1, $a2, $a3, $a4, $a5, $a6);
 };
 $runFn8 = function($fn = null, $a0 = null, $a1 = null, $a2 = null, $a3 = null, $a4 = null, $a5 = null, $a6 = null, $a7 = null) use (&$runFn8) {
-    if (func_num_args() < 9) {
-        $__args = func_get_args();
+    if (\func_num_args() < 9) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$runFn8) {
-            return $runFn8(...array_merge($__args, $more));
+            return $runFn8(...\array_merge($__args, $more));
         };
     }
     return $fn($a0, $a1, $a2, $a3, $a4, $a5, $a6, $a7);
 };
 $runFn9 = function($fn = null, $a0 = null, $a1 = null, $a2 = null, $a3 = null, $a4 = null, $a5 = null, $a6 = null, $a7 = null, $a8 = null) use (&$runFn9) {
-    if (func_num_args() < 10) {
-        $__args = func_get_args();
+    if (\func_num_args() < 10) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$runFn9) {
-            return $runFn9(...array_merge($__args, $more));
+            return $runFn9(...\array_merge($__args, $more));
         };
     }
     return $fn($a0, $a1, $a2, $a3, $a4, $a5, $a6, $a7, $a8);
 };
 $runFn10 = function($fn = null, $a0 = null, $a1 = null, $a2 = null, $a3 = null, $a4 = null, $a5 = null, $a6 = null, $a7 = null, $a8 = null, $a9 = null) use (&$runFn10) {
-    if (func_num_args() < 11) {
-        $__args = func_get_args();
+    if (\func_num_args() < 11) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$runFn10) {
-            return $runFn10(...array_merge($__args, $more));
+            return $runFn10(...\array_merge($__args, $more));
         };
     }
     return $fn($a0, $a1, $a2, $a3, $a4, $a5, $a6, $a7, $a8, $a9);
